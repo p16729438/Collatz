309a88cedf3a0257a0ed4f0d0cea7acc4893aa1e
p16729438 <p16729438@gmail.com>
2023-04-12 16:32:43 +0900

    fix

--- src/com/p16729438/Collatz/Main.java
+++ src/com/p16729438/Collatz/Main.java
@@ -19,6 +19,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Thread Count: " + ThreadCount);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         int a = 4;
         int b = 2;
         PowNumbers.add(TWO.pow(0));
